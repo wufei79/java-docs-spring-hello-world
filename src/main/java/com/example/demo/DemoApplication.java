7cb974826aace08695c6a1772db5c82ddbfc9853
wufei79 <4823079@qq.com>
2023-04-03 16:50:48 +0800

    fix bug

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dto.*;
+import com.example.util.*;
 
 @SpringBootApplication
 @RestController
@@ -26,10 +32,12 @@ public class DemoApplication {
 	@RequestMapping(value="/question",method = RequestMethod.POST)
     @ResponseBody
     public ResultModel getAnswer(@RequestBody QuestionDTO questionDTO){
-		if (null==quesitonDTO) 
+		if (null==questionDTO) {
 			return ResultModel.error("questionDTO is null");
-		if (null==questionDTO.getQuestion())	
+		}
+		if (null==questionDTO.getQuestion()) {
 			return ResultModel.error("question is null");
-		return questionDTO.getQuestion;
+		}
+		return ResultModel.success(questionDTO.getQuestion());
 	}
 }
