a6c4f318584cc78e3f0299b10c22629bb31bac84
wufei79 <4823079@qq.com>
2023-04-04 11:22:16 +0800

    fix

--- src/main/java/com/example/demo/Ask.java
+++ src/main/java/com/example/demo/Ask.java
@@ -8,10 +8,14 @@ import com.example.openai.completion.chat.ChatMessage;
 import com.example.openai.completion.chat.ChatMessageRole;
 import com.example.service.OpenAiService;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class Ask {
+    static List<ChatMessage> conversation = new LinkedList<ChatMessage>();
+
     public static String askCompletionQuestion(String token, String question, Double temperature, int maxTokens) {
         String answer;
 
@@ -23,7 +27,7 @@ public class Ask {
                 .echo(true)
                 .temperature(temperature)
                 .topP(1.0)
-                .stop(null)
+                .stop(Arrays.asList("\n","\n"))
                 .maxTokens(maxTokens)
                 .build();
         
@@ -42,16 +46,17 @@ public class Ask {
     }
 
     public static String askChatQuestion(String token, String question, Double temperature, int maxTokens) {
-        String answer;
+        String answer = "";
 
         OpenAiService service = new OpenAiService(token);
 
-        List<ChatMessage> messages = new LinkedList<ChatMessage>();
-        messages.add(new ChatMessage(ChatMessageRole.USER.value(), question));
+        //List<ChatMessage> messages = new LinkedList<ChatMessage>();
+        //messages.add(conversation.forEach(null));
+        conversation.add(new ChatMessage(ChatMessageRole.USER.value(), question));
 
         ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
                 .model("gpt-3.5-turbo")
-                .messages(messages)
+                .messages(conversation)
                 .temperature(temperature)
                 .topP(1.0)
                 .stop(null)
@@ -61,9 +66,16 @@ public class Ask {
         //service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
         try {
             java.util.List<ChatCompletionChoice> choices = service.createChatCompletion(chatCompletionRequest).getChoices();
-            answer = choices.get(0).getMessage().getContent();
+            if (null != choices.get(0)) {
+                conversation.remove(conversation.size()-1);
+
+                answer = choices.get(0).getMessage().getContent();
+                conversation.add(new ChatMessage(ChatMessageRole.ASSISTANT.value(), answer));
+            }
             System.out.println("Answer has " + choices.size() + " choices");
         } catch(Exception ex) {
+            conversation.remove(conversation.size()-1);
+
             ex.printStackTrace();
             answer = ex.getMessage();
         }
